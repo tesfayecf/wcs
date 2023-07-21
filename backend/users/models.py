1984595ca925e7c774a84392ba97777403a62a0e
Tesfaye Carreras <tesfayecarreras02@gmail.com>
2023-07-21 19:14:00 +0200

    new models

--- backend/users/models.py
+++ backend/users/models.py
@@ -10,18 +10,14 @@ class UserAccountManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
         if not email:
             raise ValueError('Users must have an email address')
-
         email = self.normalize_email(email)
         email = email.lower()
-
         user = self.model(
             email=email,
             **kwargs
         )
-
         user.set_password(password)
         user.save(using=self._db)
-
         return user
 
     def create_superuser(self, email, password=None, **kwargs):
@@ -30,12 +26,33 @@ class UserAccountManager(BaseUserManager):
             password=password,
             **kwargs
         )
-
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-
         return user
+    
+    def get_user_by_email(self, email):
+        try:
+            return self.get(email__iexact=email)
+        except self.model.DoesNotExist:
+            return None
+
+    def search_users(self, search_query):
+        return self.filter(email__icontains=search_query)
+
+    def active_users(self):
+        return self.filter(is_active=True)
+
+    def staff_users(self):
+        return self.filter(is_staff=True)
+
+    def superusers(self):
+        return self.filter(is_superuser=True)
+
+    def get_users_with_custom_field(self, field_name, field_value):
+        query = {field_name: field_value}
+        return self.filter(**query)
+
 
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
@@ -51,6 +68,12 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
+        return self.first_name
 
     def __str__(self):
         return self.email

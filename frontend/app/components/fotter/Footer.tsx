64d0b763f584bc2d914c7dd47e6b67cc166e87d7
Tesfaye Carreras <tesfayecarreras02@gmail.com>
2023-08-04 19:16:09 +0200

    ...

--- frontend/app/components/fotter/Footer.tsx
+++ frontend/app/components/fotter/Footer.tsx
@@ -4,7 +4,7 @@ import style from "./styles/Footer.module.scss"
 
 const Footer: React.FunctionComponent<{}> = () => (
     <div className={style.footer}>
-        <p className={style.footer_text}>
+        <p className={style.text}>
             © 2023 All rights reserved by WCS
         </p>
     </div>

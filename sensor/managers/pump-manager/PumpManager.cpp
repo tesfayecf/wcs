ed8651702523e6de2f2a5c87d719828e7ea97e22
Tesfaye Carreras <tesfayecarreras02@gmail.com>
2023-08-10 12:57:50 +0200

    Pump manager logic -> static variables

--- sensor/managers/pump-manager/PumpManager.cpp
+++ sensor/managers/pump-manager/PumpManager.cpp
@@ -4,6 +4,10 @@
 
 PumpManager::PumpManager(App& app) : appInstance(app) {}
 
+bool PumpManager::pumpActive = false;
+bool PumpManager::startButtonState = false;
+bool PumpManager::stopButtonState = false;
+
 void PumpManager::setup() {
     Serial.println("Initializing PumpManager");
     // // Set pin I/O
@@ -22,97 +26,47 @@ void PumpManager::setup() {
 }
 
 void PumpManager::loop() {
-    int value;
-    value = digitalRead(START_PIN);
-    if (value == HIGH) {
-        digitalWrite(STOP_LED_PIN, LOW);
+    int start;
+    int stop;
+    start = digitalRead(START_PIN);
+    stop = digitalRead(STOP_PIN);
+
+    if (start != PumpManager::startButtonState) {
+        if (start == HIGH) {
+            if (!PumpManager::pumpActive) {
+                digitalWrite(RELE_PIN, HIGH);
+                PumpManager::pumpActive = true;
+            }
+            PumpManager::startButtonState = true;
+        }
+        if (start == LOW) {
+            PumpManager::startButtonState = false;
+        }
+    }
+
+    if (stop != PumpManager::stopButtonState) {
+        if (stop == HIGH) {
+            if (pumpActive) {
+                digitalWrite(RELE_PIN, LOW);
+                pumpActive = false;
+            }
+            PumpManager::stopButtonState = true;
+        }
+
+        if (stop == LOW) {
+            PumpManager::stopButtonState = false;
+        }
+    }
+
+    if (pumpActive) {
         digitalWrite(START_LED_PIN, HIGH);
-    } else if (value == LOW) {
-        digitalWrite(STOP_LED_PIN, HIGH);
+        digitalWrite(STOP_LED_PIN, LOW);
+    } else {
+        if (PumpManager::stopButtonState) {
+            digitalWrite(STOP_LED_PIN, HIGH);
+        } else {
+            digitalWrite(STOP_LED_PIN, LOW);
+        }
         digitalWrite(START_LED_PIN, LOW);
     }
 }
-
-// // if stop is not pressed and we press start, start the pump
-// if (digitalRead(STOP_PIN) == LOW) {
-//     if (digitalRead(START_PIN) == HIGH) {
-//         digitalWrite(RELE_PIN, LOW);
-//         digitalWrite(START_LED_PIN, LOW);
-//         digitalWrite(STOP_LED_PIN, HIGH);
-//     }
-// }
-
-// // if stop is pressed and we start is pressed, stop the pump
-// if (digitalRead(STOP_PIN) == LOW) {
-//     if (digitalRead(START_PIN) == LOW) {
-//         digitalWrite(RELE_PIN, HIGH);
-//         digitalWrite(START_LED_PIN, HIGH);
-//         digitalWrite(STOP_LED_PIN, LOW);
-//         delay(200);
-//     }
-// }
-
-// // if stop is pressed and start is not pressed, reset the stop state
-// if (digitalRead(STOP_PIN) == HIGH) {
-//     if (digitalRead(START_PIN) == HIGH) {
-//         digitalWrite(RELE_PIN, HIGH);
-//         digitalWrite(START_LED_PIN, HIGH);
-//         digitalWrite(STOP_LED_PIN, HIGH);
-//     }
-// }
-
-// // Read button states
-//     startButtonState = digitalRead(START_PIN);
-//     stopButtonState = digitalRead(STOP_PIN);
-//     unsigned long currentTime = millis();
-
-//     // Check if the start button is pressed and handle debounce
-//     if (startButtonState != prevStartButtonState) {
-//         lastDebounceTime = currentTime;
-//     }
-//     if ((currentTime - lastDebounceTime) > DEBOUNCE_TIME) {
-//         // If the button state has changed and is stable for a certain
-//         time if (startButtonState == LOW && !prevStartButtonState) {
-//             startButtonPressed = true;
-//         }
-//     }
-//     prevStartButtonState = startButtonState;
-
-//     // Check if the stop button is pressed and handle debounce
-//     if (stopButtonState != prevStopButtonState) {
-//         lastDebounceTime = currentTime;
-//     }
-//     if ((currentTime - lastDebounceTime) > DEBOUNCE_TIME) {
-//         // If the button state has changed and is stable for a certain
-//         time if (stopButtonState == LOW && !prevStopButtonState) {
-//             stopButtonPressed = true;
-//         }
-//     }
-//     prevStopButtonState = stopButtonState;
-
-//     // Start the pump if stop is not pressed and start is pressed
-//     if (stopButtonState == HIGH && startButtonPressed && !pumpActive) {
-//         pumpActive = true;
-//         digitalWrite(RELE_PIN, HIGH);
-//         digitalWrite(START_LED_PIN, HIGH);
-//         digitalWrite(STOP_LED_PIN, LOW);
-//     }
-
-//     // Stop the pump if stop is pressed and start is pressed
-//     if (stopButtonState == HIGH && startButtonPressed && pumpActive) {
-//         pumpActive = false;
-//         digitalWrite(RELE_PIN, LOW);
-//         digitalWrite(START_LED_PIN, LOW);
-//         digitalWrite(STOP_LED_PIN, HIGH);
-//         delay(200);
-//     }
-
-//     // Reset states if stop is pressed and start is not pressed
-//     if (stopButtonState == LOW && startButtonPressed) {
-//         pumpActive = false;
-//         digitalWrite(RELE_PIN, HIGH);
-//         digitalWrite(START_LED_PIN, HIGH);
-//         digitalWrite(STOP_LED_PIN, HIGH);
-//         startButtonPressed = false;
-//         stopButtonPressed = false;
-//     }
--- sensor/managers/pump-manager/PumpManager.h
+++ sensor/managers/pump-manager/PumpManager.h
@@ -6,18 +6,9 @@ class PumpManager {
    private:
     App& appInstance;
 
-    // Button debounce time in milliseconds
-    const unsigned long DEBOUNCE_TIME = 200;
-
-    bool pumpActive = false;  // Pump state
-    // bool startButtonState = false;      // Start button state
-    // bool stopButtonState = false;       // Stop button state
-    bool prevStartButtonState = false;  // Previous start button state
-    bool prevStopButtonState = false;   // Previous stop button state
-
-    unsigned long lastDebounceTime = 0;  // Last debounce time
-    bool startButtonPressed = false;     // Start button pressed state
-    bool stopButtonPressed = false;      // Stop button pressed state
+    static bool pumpActive;
+    static bool startButtonState;
+    static bool stopButtonState;
 
    public:
     PumpManager(App& app);

e93afa2d20be79c3a97c3d0bfa82929f5a0d537a
Tesfaye Carreras <tesfayecarreras02@gmail.com>
2023-11-08 21:46:50 +0100

    Update urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('create/', CustomTokenObtainPairView.as_view()),
+    path('login/', CustomTokenLoginView.as_view()),
     path('refresh/', CustomTokenRefreshView.as_view()),
     path('verify/', CustomTokenVerifyView.as_view()),
-    path('logout/', LogoutView.as_view()),
+    path('logout/', CustomTokenLogoutView.as_view()),
     
     path('user/', UserView.as_view()),
 ]

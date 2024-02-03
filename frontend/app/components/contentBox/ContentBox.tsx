631d8ef0d39772a7735cde0ea32ec6e8aacfbeb4
Tesfaye Carreras <tesfayecarreras02@gmail.com>
2024-02-03 13:47:33 +0100

    frontend: content box funcionalities

--- frontend/app/components/contentBox/ContentBox.tsx
+++ frontend/app/components/contentBox/ContentBox.tsx
@@ -3,10 +3,28 @@ import React, { ReactNode } from "react";
 type IContentBoxProps = {
     children: ReactNode;
     customBoxClass?: string;
+    isLoading?: boolean;
+    isError?: boolean;
 };
 
 const ContentBox: React.FunctionComponent<IContentBoxProps> = (props) => {
     const boxClassName = `${"content_box"} ${props.customBoxClass || ""}`;
+
+    if (props.isLoading) {
+        return <div className="content-box-loading">
+            <div className={"loading"}>
+                Loading...
+            </div>
+        </div>;
+    }
+
+    if (props.isError) {
+        return <div className="content-box-error">
+            <div className={"error"}>
+                Error
+            </div>
+        </div>;
+    }
 
     return (
         <div id="content-box" className={boxClassName}>

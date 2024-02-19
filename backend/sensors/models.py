f73ec428dc694115e2c596458af4e424d9563c3f
Tesfaye Carreras <tesfayecarreras02@gmail.com>
2024-02-19 01:23:15 +0100

    backend: sensor app models with timescale database

--- backend/sensors/models.py
+++ backend/sensors/models.py
@@ -1,37 +1,51 @@
 from django.db import models
-from enum import Enum
+
+from timescale.db.models.fields import TimescaleDateTimeField
+from timescale.db.models.managers import TimescaleManager
+from django.utils.timezone import now
 
 
-###############
-### READING ###
-###############
-class SensorReading(models.Model):
-    timestamp = models.DateTimeField(auto_now=True)
-    level = models.FloatField()
+class TimescaleModel(models.Model):
+    """
+    A helper class for using Timescale within Django, has the TimescaleManager and
+    TimescaleDateTimeField already present. This is an abstract class it should
+    be inheritted by another class for use.
+    """
+    time = TimescaleDateTimeField(interval="1 minute",default=now) # 1 minute
+    objects = TimescaleManager()
+
+    class Meta:
+        abstract = True
+
+
+# ###############
+# ### READING ###
+# ###############
+class SensorReading(TimescaleModel):
+    level = models.FloatField(null=True, blank=True)
     temperature = models.FloatField(null=True, blank=True)
     humidity = models.FloatField(null=True, blank=True)
-    sensor = models.ForeignKey('data.Sensor', on_delete=models.CASCADE, related_name='readings')
+    sensor_id = models.IntegerField(default=-1)
     
     def __str__(self):
-        return f"Timestamp: {self.timestamp} Sensor: {self.sensor.token}"
+        return f"Sensor: {self.sensor_id} "
 
 
-###########
-### LOG ###
-###########
+# ###########
+# ### LOG ###
+# ###########
 class SensorStatus(models.TextChoices):
     NORMAL = ('Normal', 'Normal')
     WARNING = ('Warning', 'Warning')
     ERROR = ('Error', 'Error')
 
-class SensorLog(models.Model):
-    timestamp = models.DateTimeField(auto_now=True)
+class SensorLog(TimescaleModel):
     status = models.CharField(max_length=25, choices=SensorStatus.choices, default=SensorStatus.NORMAL)
+    status_message = models.TextField(null=True, blank=True)
     signal_strength = models.IntegerField(null=True, blank=True)
     battery_voltage = models.FloatField(null=True, blank=True)
-    is_error = models.BooleanField(default=False)
-    error_message = models.TextField(null=True, blank=True)
-    sensor = models.ForeignKey('data.Sensor', on_delete=models.CASCADE, related_name='logs')
+    battery_percentage = models.IntegerField(null=True, blank=True)
+    sensor_id = models.IntegerField(default=-1)
 
     def __str__(self):
-        return f"Timestamp: {self.timestamp} Sensor: {self.sensor.token} Status: {self.status}"
+        return f"Sensor: {self.sensor_id} Status: {self.status}"

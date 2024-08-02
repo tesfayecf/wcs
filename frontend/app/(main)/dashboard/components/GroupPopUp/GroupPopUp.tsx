478d26a52bf2190597b9819cbb521f3cdfb43bad
tesfayecf <90719152+tesfayecf@users.noreply.github.com>
2024-08-02 21:32:48 +0200

    frontend: group popup

--- frontend/app/(main)/dashboard/components/GroupPopUp/GroupPopUp.tsx
+++ frontend/app/(main)/dashboard/components/GroupPopUp/GroupPopUp.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react';
-import FormTemplate from '@/app/components/formTemplate/FormTemplate';
-import PopUpTemplate from '@/app/components/popUpTemplate/PopUpTemplate';
+import Form from '@/app/components/form/Form';
+import Popup from '@/app/components/popup/Popup';
 import { IGroupCreationForm } from '@/app/(main)/dashboard/types';
 import useDashboardStore from '@/app/(main)/dashboard/store';
 import { createGroup, deleteGroup, editGroup, getGroups } from '../../actions';
@@ -17,74 +17,73 @@ const GroupPopUp: React.FunctionComponent<IFroupPopUpProps> = (props: IFroupPopU
     if (groupMenu.mode == "edit" || groupMenu.mode == "delete") {
         group = groups.find(g => g.id == groupMenu.id);
         if (!group) {
-            setGroupMenu({ id: -1, mode: "", show: false })
-            return <></>
+            setGroupMenu({ id: -1, mode: "", show: false });
+            console.error("Grup not found");
+            return null;
         }
     }
 
     const onAccept = async (fields: IGroupCreationForm) => {
         let response = undefined;
-        if (groupMenu.mode == "create") {
-            response = await createGroup(fields);
-            if (response.ok) await getGroups() // Update groups RTQ query
-        } else if (groupMenu.mode == "edit") {
-            response = await editGroup(groupMenu.id, fields);
-            if (response.ok) await getGroups() // Update groups
-        } else if (groupMenu.mode == "delete") {
-            response = await deleteGroup(groupMenu.id);
-        }
+        if (groupMenu.mode == "create") response = await createGroup(fields);
+        else if (groupMenu.mode == "edit") response = await editGroup(groupMenu.id, fields);
+        else if (groupMenu.mode == "delete") response = await deleteGroup(groupMenu.id);
 
-        setGroupMenu({ id: -1, mode: "", show: false })
-        if (response.ok) await getGroups() // Update groups
-        else console.log("Error") // Show error message
+        setGroupMenu({ id: -1, mode: "", show: false });
+        if (response.ok) await getGroups(); // Update groups
+        else console.error("Error when executing form action"); // Show error message
+    }
 
+    const onCancel = () => {
+        setGroupMenu({ id: -1, mode: "", show: false });
     }
 
     return (
-        <PopUpTemplate
+        <Popup
             open={groupMenu.show}
             onClose={() => setGroupMenu({ id: -1, mode: "", show: false })
             }
         >
-            <FormTemplate<IGroupCreationForm>
-                title="Create Group"
-                externalError={false}
-                externalErrorText={"Invalid data"}
-                onCancel={() => setGroupMenu({ id: -1, mode: "", show: false })}
-                onAccept={onAccept}
-                acceptButton={groupMenu.mode.toLocaleUpperCase()}
-                cancelButton="Cancel"
-                showCancelButton={true}
-                isLoading={false}
-                fields={[
-                    {
-                        key: "name",
-                        name: "Name",
-                        type: "text",
-                        placeholder: "",
-                        defaultValue: group?.name,
-                    },
-                    {
-                        key: "location",
-                        name: "Location",
-                        type: "text",
-                        placeholder: "",
-                        defaultValue: group?.location,
-                    },
-                    {
-                        key: "description",
-                        name: "Description",
-                        type: "multitext",
-                        placeholder: "",
-                        rows: 3,
-                        defaultValue: group?.description,
-                    },
-                ]}
-            />
-        </PopUpTemplate>
+            {groupMenu.mode == "delete" ?
+                null
+                :
+                <Form<IGroupCreationForm>
+                    title="Create Group"
+                    externalError={false}
+                    externalErrorText={"Invalid data"}
+                    onCancel={onCancel}
+                    onAccept={onAccept}
+                    acceptButton={groupMenu.mode.toLocaleUpperCase()}
+                    cancelButton="Cancel"
+                    showCancelButton={true}
+                    isLoading={false}
+                    fields={[
+                        {
+                            key: "name",
+                            name: "Name",
+                            type: "text",
+                            placeholder: "",
+                            defaultValue: group?.name,
+                        },
+                        {
+                            key: "location",
+                            name: "Location",
+                            type: "text",
+                            placeholder: "",
+                            defaultValue: group?.location,
+                        },
+                        {
+                            key: "description",
+                            name: "Description",
+                            type: "multitext",
+                            placeholder: "",
+                            rows: 3,
+                            defaultValue: group?.description,
+                        },
+                    ]}
+                />}
+        </Popup>
     )
 };
 
 export default GroupPopUp;
-
-

f5c50ac2fa2ed2c6d50357996a0566eb0d3cc678
tesfayecf <90719152+tesfayecf@users.noreply.github.com>
2024-08-02 23:09:47 +0200

    frontend: test page

--- frontend/app/(test)/test/page.tsx
+++ frontend/app/(test)/test/page.tsx
@@ -1,11 +1,36 @@
 'use client'
 import React from "react";
+import CustomForm from "@/app/components/form/CustomForm";
+import { ILoginForm } from "@/app/(auth)/types";
 
 interface ITestProps { }
 
 const Test: React.FunctionComponent<ITestProps> = (props: ITestProps) => {
     return (
-        <></>
+        <>
+            <CustomForm<ILoginForm>
+                open={true}
+                title={"LOG IN. BE THE CHANGE"}
+                onAccept={() => { }}
+                acceptText="Log in"
+                onCancel={() => { }}
+                cancelText="Cancel"
+                closable={false}
+                footerClassName="footter"
+                fields={[
+                    {
+                        key: "email",
+                        type: "text",
+                        label: "Email",
+                    },
+                    {
+                        key: "password",
+                        type: "text",
+                        label: "Password",
+                    }
+                ]}
+            />
+        </>
     )
 }
 

369f34a781831ea185af24734f7f75a88f75bc90
tesfayecf <90719152+tesfayecf@users.noreply.github.com>
2024-11-09 22:36:22 +0100

    frontend: edit timeseries dummy data creation

--- backend/timeseries/management/commands/create_timeseries_data.py
+++ backend/timeseries/management/commands/create_timeseries_data.py
@@ -27,53 +27,46 @@ class Command(BaseCommand):
                 )
                 self.stdout.write(self.style.SUCCESS(f"Created {measure}"))
 
-                # Create dummy channels for each measure
-                for version in range(1, 4):  # Create 3 versions of each channel
-                    channel = Channel.objects.create(
-                        label=f"Channel-{measure.label}-{version}",
-                        version=f"v{version}",
-                        unit=random.choice(['Celsius', 'Bar', 'Meters']),
-                        rate=random.choice([1, 10, 60]),  # Sampling rates in seconds
-                        measure=measure
+                # Create dummy channel for each measure
+                channel = Channel.objects.create(
+                    label=f"Channel-{measure.label}",
+                    version=f"v1",
+                    unit=random.choice(['Celsius', 'Bar', 'Meters']),
+                    rate=random.choice([1, 10, 60]),  # Sampling rates in seconds
+                    measure=measure
+                )
+                self.stdout.write(self.style.SUCCESS(f"Created {channel}"))
+
+                # Create time chunks and records for 1 minute time intervals
+                start_time = now() - timedelta(days=5)  # Start from 5 days ago
+                end_time = now()
+
+                interval = timedelta(minutes=1)
+                chunk_start_time = start_time
+                chunk_end_time = chunk_start_time + interval * 30
+
+                while chunk_end_time <= end_time:
+                    # Create a time chunk
+                    chunk = Chunk.objects.create(
+                        measure=measure,
+                        start_time=chunk_start_time,
+                        end_time=chunk_end_time
                     )
-                    self.stdout.write(self.style.SUCCESS(f"Created {channel}"))
+                    self.stdout.write(self.style.SUCCESS(f"Created {chunk}"))
 
-                    # Create time chunks and records for different time intervals
-                    time_intervals = {
-                        'minutes': timedelta(minutes=1),
-                        'hours': timedelta(hours=1),
-                        'daily': timedelta(days=1),
-                    }
-
-                    start_time = now() - timedelta(days=30)  # Start from 30 days ago
-                    end_time = now()
-
-                    interval = random.choice(list(time_intervals.items()))
-                    chunk_start_time = start_time
-                    chunk_end_time = chunk_start_time + interval[1] * 30
-
-                    while chunk_end_time <= end_time:
-                        # Create a time chunk
-                        chunk = Chunk.objects.create(
-                            measure=measure,
-                            start_time=chunk_start_time,
-                            end_time=chunk_end_time
+                    # Generate records for the chunk at the specified time rate
+                    record_time = chunk_start_time
+                    while record_time <= chunk_end_time:
+                        record_value = random.uniform(0, 100)  # Random float values for records
+                        Record.objects.create(
+                            time=record_time,
+                            value=record_value,
+                            channel=channel,
+                            chunk=chunk
                         )
-                        self.stdout.write(self.style.SUCCESS(f"Created {chunk}"))
+                        record_time += interval  # Move to next timestamp based on interval
 
-                        # Generate records for the chunk at the specified time rate
-                        record_time = chunk_start_time
-                        while record_time <= chunk_end_time:
-                            record_value = random.uniform(0, 100)  # Random float values for records
-                            Record.objects.create(
-                                time=record_time,
-                                value=record_value,
-                                channel=channel,
-                                chunk=chunk
-                            )
-                            record_time += interval[1]  # Move to next timestamp based on interval
-
-                        chunk_start_time = chunk_end_time
-                        chunk_end_time = chunk_start_time + interval[1] * random.randint(10, 100)
+                    chunk_start_time = chunk_end_time
+                    chunk_end_time = chunk_start_time + interval * random.randint(10, 100)
 
         self.stdout.write(self.style.SUCCESS("Dummy time-series data generation completed."))
